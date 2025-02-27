00e180bc741849702f2797edfca73565161a2aa4
bizarai <134594060+bizarai@users.noreply.github.com>
2025-02-27 11:24:37 +0800

    Update page.tsx

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -52,7 +52,7 @@ function HelloWorld() {
       </div>
 
       <div className="fixed bottom-4 right-4">
-        import Link from 'next/link';
+      import Link from 'next/link';
         <Link
           href="/"
           className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
