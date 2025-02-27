b61bd8bf3717981957cfc762537a329898b3ad13
bizarai <134594060+bizarai@users.noreply.github.com>
2025-02-27 16:58:46 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  };
-  
-  module.exports = nextConfig;  
+  reactStrictMode: true,
+  output: 'export',  // Static HTML export
+  basePath: '/snippet-manager', // Replace with your actual repo name
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
